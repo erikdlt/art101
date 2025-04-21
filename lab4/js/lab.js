1bb2f87af2913ed5ef689c9ee536edb929756638
erikdlt <erikwiitime@gmail.com>
2025-04-21 13:43:38 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// An implementation of that game
+// Author: Erik De La Trinidad <edelatri@ucsc.edu>
+// Date: April 2025
+
+//Prompt user to make a selection
+//Accept user input
+//Generate computer’s move randomly
+    //random selection (rock, paper, scissors)
+//If same move, print tie result
+//If not tie, print winner’s move and results screen
+
 
 // Constants
 
