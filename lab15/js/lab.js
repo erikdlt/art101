b64046e7529b91d43ade0c31f1eb5389de296b48
erikdlt <erikwiitime@gmail.com>
2025-06-02 11:11:47 -0700

    lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -22,9 +22,20 @@ $("#activate").click(function() {
         console.log("Success!");
         console.log(data);
         // extract the pokemon info
-        let pokemon = data.pokemon;
+        let name = data.name;
+        let id = data.id
+        let type = data.types[0].type.name;
+        let weight = data.weight;
+        let imageUrl = data.sprites.front_default;
 
         //insert output
+        let output = `
+        <h1>${name.toUpperCase()}</h1>
+            <img src="${imageUrl}" alt="${name}">
+            <p><strong>ID:</strong> ${id}</p>
+            <p><strong>Type:</strong> ${type}</p>
+            <p><strong>Weight:</strong> ${weight}</p>
+        `;
         $("#output").html("<h1>" + pokemon);
     })
 
